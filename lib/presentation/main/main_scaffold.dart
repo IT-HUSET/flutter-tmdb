15697c3129275ad18f36254fa0d68628a82be944
Tobias Löfstrand <tobias@leafnode.se>
2023-04-26 14:20:11 +0200

    Some simplification and cleanup

--- lib/presentation/main/main_scaffold.dart
+++ lib/presentation/main/main_scaffold.dart
@@ -11,22 +11,24 @@ class MainScaffold extends StatefulWidget {
 }
 
 class _MainScaffoldState extends State<MainScaffold> {
-  /// MaterialApp builds a HeroController by default for the root Navigator, but since we build navigators on our own,
-  /// we need to build a HeroController for each Navigator.
-  final _heroController = List.generate(3, (_) => MaterialApp.createMaterialHeroController());
+  /// MaterialApp builds a HeroController by default for the root Navigator, but since we're using a nested Navigator,
+  /// we need to build a HeroController for that on our own.
+  final _heroController = MaterialApp.createMaterialHeroController();
+
+  String _initialLocation(int index) {
+    switch (index) {
+      case 1: return 'favorites';
+      case 2: return 'settings';
+      default: return 'movies';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final appState = AppState.of(context);
 
     return Scaffold(
-      body: Stack(
-        children: [
-          _navigator(context, appState, 'movies', 0),
-          _navigator(context, appState, 'favorites', 1),
-          _navigator(context, appState, 'settings', 2),
-        ],
-      ),
+      body: _navigator(context, appState),
       bottomNavigationBar: MainBottomBar(
         currentIndex: appState.mainSectionIndex,
         onTap: (index) => appState.mainSectionIndex = index,
@@ -34,23 +36,19 @@ class _MainScaffoldState extends State<MainScaffold> {
     );
   }
 
-  Widget _navigator(BuildContext context, AppState appState, String initialRoute, int index) {
+  Widget _navigator(BuildContext context, AppState appState) {
     final appRouter = appState.appRouter;
 
     /// Define a scope for hero transitions - needs to be done for each Navigator.
     return HeroControllerScope(
-      controller: _heroController[index],
-
-      /// We use Offstage to hide the Navigator that is not currently active.
-      child: Offstage(
-        offstage: index != appState.mainSectionIndex,
-        child: Navigator(
-          initialRoute: initialRoute,
-          onGenerateRoute: (settings) {
-            final String name = settings.name ?? '';
-            return MaterialPageRoute(settings: settings, builder: appRouter.routes[name]!);
-          },
-        ),
+      controller: _heroController,
+      child: Navigator(
+        key: ValueKey('Navigator-section-${appState.mainSectionIndex}'),
+        initialRoute: _initialLocation(appState.mainSectionIndex),
+        onGenerateRoute: (settings) {
+          final String name = settings.name ?? '';
+          return MaterialPageRoute(settings: settings, builder: appRouter.routes[name]!);
+        },
       ),
     );
   }
